480ee777eb070803f671e129dfe4625059a467ba
hyejinme <kimhyejin703@daum.net>
2023-01-06 00:37:36 +0900

    백엔드 서버 초기 세팅

---
+++ server/src/main/java/com/newyear/mainproject/HomeController.java
@@ -0,0 +1,12 @@
+package com.newyear.mainproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "test";
+    }
+}
