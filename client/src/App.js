c063c77fb0554924b8b7ca20d6e5553ff093d03c
seungheonkim <seungheon818@naver.com>
2023-01-06 15:20:06 +0900

    feat: router작업 및 폴더 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import {useState} from "react";
+import {Route, Routes} from "react-router-dom";
+
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
-        <div></div>
+        <Routes>
+            <Route path="/" element={isLoggedIn ? <Main/> : <Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/user/:memberId" element={<UserProfile/>}/>
+            <Route path="/user/:memberId/edit" element={<UserProfileEdit/>}/>
+            <Route path="/itinerary/:itineraryId" element={<Itinerary/>}/>
+            <Route path="/blog" element={<Blog/>}/>
+        </Routes>
     );
 }
 
