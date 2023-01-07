50d4fcc6b698d4fcdf99e87a85722e7da37645da
phant <phantom4060@gmail.com>
2023-01-07 21:18:00 +0900

    feat: 로그아웃 기능 추가

--- server/src/main/java/com/newyear/mainproject/security/filter/JwtVerificationFilter.java
+++ server/src/main/java/com/newyear/mainproject/security/filter/JwtVerificationFilter.java
@@ -1,7 +1,14 @@
 package com.newyear.mainproject.security.filter;
 
+import com.newyear.mainproject.exception.BusinessLogicException;
+import com.newyear.mainproject.exception.ExceptionCode;
 import com.newyear.mainproject.security.jwt.JwtTokenizer;
+import com.newyear.mainproject.security.logout.RedisUtil;
+import com.newyear.mainproject.security.logout.RefreshTokenRepository;
 import com.newyear.mainproject.security.utils.CustomAuthorityUtils;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,20 +23,51 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class JwtVerificationFilter extends OncePerRequestFilter{
     private final JwtTokenizer jwtTokenizer;
     private final CustomAuthorityUtils authorityUtils;
+    private final RedisUtil redisUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public JwtVerificationFilter(JwtTokenizer jwtTokenizer,
-                                 CustomAuthorityUtils authorityUtils) {
+                                 CustomAuthorityUtils authorityUtils,
+                                 RedisUtil redisUtil, RefreshTokenRepository refreshTokenRepository) {
         this.jwtTokenizer = jwtTokenizer;
         this.authorityUtils = authorityUtils;
+        this.redisUtil = redisUtil;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Map<String, Object> claims = verifyJws(request);
-        setAuthenticationToContext(claims);
+        try {
+            Map<String, Object> claims = verifyJws(request);
+
+            if(redisUtil.hasKeyBlackList(request.getHeader("Authorization").replace("Bearer ", ""))){
+                throw new BusinessLogicException(ExceptionCode.INVALID_MEMBER_STATUS);
+            }
+
+//            TODO★★★★★★★★★★★★★★★★★★★★★재발급 부분 임시로 저장 재발급 넣어야 할부분에 넣으면 될듯★★★★★★★★★★★★★★★★★★
+
+//            if(redisUtil.hasKeyBlackList(request.getHeader("Authorization").replace("Bearer ", ""))){
+//                throw new BusinessLogicException(ExceptionCode.INVALID_MEMBER_STATUS);
+//            }
+//            else if(refreshTokenRepository.findByRefreshToken(request.getHeader("Refresh")) != null) {
+//                //블랙리스트에 저장되어있지 않고 리프레쉬 토큰이 레파지토리에 존재함으로 엑세스토큰 재발급해야함
+//                RefreshToken refreshToken = refreshTokenRepository.findByRefreshToken(request.getHeader("Refresh"));
+//            }
+
+//            TODO★★★★★★★★★★★★★★★★★★★★★재발급 부분 임시로 저장 재발급 넣어야 할부분에 넣으면 될듯★★★★★★★★★★★★★★★★★★
+
+            setAuthenticationToContext(claims);
+        } catch (SignatureException se) {
+            request.setAttribute("exception", se);
+        } catch (ExpiredJwtException ee) {
+            request.setAttribute("exception", ee);
+        } catch (Exception e) {
+            request.setAttribute("exception", e);
+        }
 
         filterChain.doFilter(request, response);
     }
