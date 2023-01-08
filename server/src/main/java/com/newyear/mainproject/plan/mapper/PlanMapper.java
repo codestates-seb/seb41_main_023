bdc8f29de638fabdd644ea7f5aa36c012c943ff6
hyejinme <kimhyejin703@daum.net>
2023-01-09 01:04:40 +0900

    feat: 일정과 회원, 장소 연결 및 특정 일정 조회 기능 추가

--- server/src/main/java/com/newyear/mainproject/plan/mapper/PlanMapper.java
+++ server/src/main/java/com/newyear/mainproject/plan/mapper/PlanMapper.java
@@ -1,9 +1,15 @@
 package com.newyear.mainproject.plan.mapper;
 
+import com.newyear.mainproject.place.dto.PlaceDto;
+import com.newyear.mainproject.place.entity.Place;
 import com.newyear.mainproject.plan.dto.PlanDto;
 import com.newyear.mainproject.plan.entity.Plan;
+import com.newyear.mainproject.plan.entity.PlanDates;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PlanMapper {
@@ -17,15 +23,45 @@ public interface PlanMapper {
 
         return plan;
     }
-    default Plan planPatchDtoToPlan(PlanDto.Patch patch) {
-        Plan plan = new Plan();
+    Plan planPatchDtoToPlan(PlanDto.Patch patch);
 
-        plan.setPlanId(patch.getPlanId());
-        plan.setPlanTitle(patch.getPlanTitle());
-        plan.setStartDate(patch.getStartDate());
-        plan.setEndDate(patch.getEndDate());
-
-        return plan;
+    PlanDto.Response planToPlanResponseDto(Plan plan);
+    default PlanDto.PlaceDetailResponse planToPlaceDetailResponseDto(Plan plan){
+        PlanDto.PlaceDetailResponse response = new PlanDto.PlaceDetailResponse();
+        response.setPlanId(plan.getPlanId());
+        response.setCityName(plan.getCityName());
+        response.setPlanTitle(plan.getPlanTitle());
+        response.setStartDate(plan.getStartDate());
+        response.setEndDate(plan.getEndDate());
+        response.setPlanDates(planDateToPlanDateResponseDtos(plan.getPlanDates()));
+        response.setPlaces(placesToPlaceResponseDtos(plan.getPlaces()));
+        return response;
     }
-    Plan planToPlanResponseDto(Plan plan);
+
+    PlanDto.PlanDatesResponse planDatesToPlanDateResponseDto(PlanDates planDates);
+
+    default List<PlanDto.PlanDatesResponse> planDateToPlanDateResponseDtos(List<PlanDates> planDates) {
+        return planDates
+                .stream()
+                .map(planDate -> PlanDto.PlanDatesResponse
+                        .builder()
+                        .planDateId(planDate.getPlanDateId())
+                        .planDate(planDate.getPlanDate())
+                        .build())
+                .collect(Collectors.toList());
+    }
+    default List<PlaceDto.Response> placesToPlaceResponseDtos(List<Place> places) {
+        return places
+                .stream()
+                .map(place -> PlaceDto.Response
+                        .builder()
+                        .placeId(place.getPlaceId())
+                        .placeName(place.getPlaceName())
+                        .expense(place.getExpense())
+                        .startTime(place.getStartTime())
+                        .endTime(place.getEndTime())
+                        .planDates(planDatesToPlanDateResponseDto(place.getPlanDates()))
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
