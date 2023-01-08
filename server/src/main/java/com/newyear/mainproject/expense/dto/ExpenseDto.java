4d2aa38283ff19e0de6c0736d8d91812eb2eb757
kimmj13 <qlckadk@gmail.com>
2023-01-08 22:07:34 +0900

    style : 예산과 비용 코드 스타일 변경 #10

--- server/src/main/java/com/newyear/mainproject/expense/dto/ExpenseDto.java
+++ server/src/main/java/com/newyear/mainproject/expense/dto/ExpenseDto.java
@@ -9,9 +9,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class ExpenseDto {
 
     @Getter
