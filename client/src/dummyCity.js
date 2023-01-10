f6db35c9fbed06c533675a9295ad53a277383d00
sena <senika_@naver.com>
2023-01-10 22:34:40 +0900

    feat: 비용 추가 기능

---
+++ client/src/dummyCity.js
@@ -0,0 +1,658 @@
+export const data = [
+  {
+    id: 1,
+    cityName: "서울특별시",
+  },
+  {
+    id: 2,
+    cityName: "인천광역시",
+  },
+  {
+    id: 3,
+    cityName: "세종특별자치시",
+  },
+  {
+    id: 4,
+    cityName: "대전광역시",
+  },
+  {
+    id: 5,
+    cityName: "대구광역시",
+  },
+  {
+    id: 6,
+    cityName: "울산광역시",
+  },
+  {
+    id: 7,
+    cityName: "부산광역시",
+  },
+  {
+    id: 8,
+    cityName: "광주광역시",
+  },
+  {
+    id: 9,
+    cityName: "제주특별자치도 제주시",
+  },
+  {
+    id: 10,
+    cityName: "제주특별자치도 서귀포시",
+  },
+  {
+    id: 11,
+    cityName: "울릉도",
+  },
+  {
+    id: 12,
+    cityName: "독도",
+  },
+  {
+    id: 13,
+    cityName: "경기도 시흥시",
+  },
+  {
+    id: 14,
+    cityName: "경기도 광명시",
+  },
+  {
+    id: 15,
+    cityName: "경기도 부천시",
+  },
+  {
+    id: 16,
+    cityName: "경기도 안양시",
+  },
+  {
+    id: 17,
+    cityName: "경기도 안산시",
+  },
+  {
+    id: 18,
+    cityName: "경기도 화성시",
+  },
+  {
+    id: 19,
+    cityName: "경기도 과천시",
+  },
+  {
+    id: 20,
+    cityName: "경기도 의왕시",
+  },
+  {
+    id: 21,
+    cityName: "경기도 군포시",
+  },
+  {
+    id: 22,
+    cityName: "경기도 수원시",
+  },
+  {
+    id: 23,
+    cityName: "경기도 오산시",
+  },
+  {
+    id: 24,
+    cityName: "경기도 평택시",
+  },
+  {
+    id: 25,
+    cityName: "경기도 안성시",
+  },
+  {
+    id: 26,
+    cityName: "경기도 용인시",
+  },
+  {
+    id: 27,
+    cityName: "경기도 이천시",
+  },
+  {
+    id: 28,
+    cityName: "경기도 여주시",
+  },
+  {
+    id: 29,
+    cityName: "경기도 광주시",
+  },
+  {
+    id: 30,
+    cityName: "경기도 성남시",
+  },
+  {
+    id: 31,
+    cityName: "경기도 하남시",
+  },
+  {
+    id: 32,
+    cityName: "경기도 구리시",
+  },
+  {
+    id: 33,
+    cityName: "경기도 고양시",
+  },
+  {
+    id: 34,
+    cityName: "경기도 김포시",
+  },
+  {
+    id: 35,
+    cityName: "경기도 파주시",
+  },
+  {
+    id: 36,
+    cityName: "경기도 양주시",
+  },
+  {
+    id: 37,
+    cityName: "경기도 포천시",
+  },
+  {
+    id: 38,
+    cityName: "경기도 남양주시",
+  },
+  {
+    id: 39,
+    cityName: "경기도 의정부시",
+  },
+  {
+    id: 40,
+    cityName: "경기도 동두천시",
+  },
+  {
+    id: 41,
+    cityName: "경기도 가평군",
+  },
+  {
+    id: 42,
+    cityName: "경기도 양평군",
+  },
+  {
+    id: 43,
+    cityName: "경기도 연천군",
+  },
+  {
+    id: 44,
+    cityName: "강원도 속초시",
+  },
+  {
+    id: 45,
+    cityName: "강원도 춘천시",
+  },
+  {
+    id: 46,
+    cityName: "강원도 원주시",
+  },
+  {
+    id: 47,
+    cityName: "강원도 태백시",
+  },
+  {
+    id: 48,
+    cityName: "강원도 삼척시",
+  },
+  {
+    id: 49,
+    cityName: "강원도 동해시",
+  },
+  {
+    id: 50,
+    cityName: "강원도 강릉시",
+  },
+  {
+    id: 51,
+    cityName: "강원도 철원군",
+  },
+  {
+    id: 52,
+    cityName: "강원도 화천군",
+  },
+  {
+    id: 53,
+    cityName: "강원도 양구군",
+  },
+  {
+    id: 54,
+    cityName: "강원도 고성군",
+  },
+  {
+    id: 55,
+    cityName: "강원도 인제군",
+  },
+  {
+    id: 56,
+    cityName: "강원도 양양군",
+  },
+  {
+    id: 57,
+    cityName: "강원도 홍천군",
+  },
+  {
+    id: 58,
+    cityName: "강원도 횡성군",
+  },
+  {
+    id: 59,
+    cityName: "강원도 평창군",
+  },
+  {
+    id: 60,
+    cityName: "강원도 정선군",
+  },
+  {
+    id: 61,
+    cityName: "강원도 영월군",
+  },
+  {
+    id: 62,
+    cityName: "충청남도 당진시",
+  },
+  {
+    id: 63,
+    cityName: "충청남도 서산시",
+  },
+  {
+    id: 64,
+    cityName: "충청남도 아산시",
+  },
+  {
+    id: 65,
+    cityName: "충청남도 천안시",
+  },
+  {
+    id: 66,
+    cityName: "충청남도 공주시",
+  },
+  {
+    id: 67,
+    cityName: "충청남도 논산시",
+  },
+  {
+    id: 68,
+    cityName: "충청남도 계룡시",
+  },
+  {
+    id: 69,
+    cityName: "충청남도 보령시",
+  },
+  {
+    id: 70,
+    cityName: "충청남도 태안군",
+  },
+  {
+    id: 71,
+    cityName: "충청남도 예산군",
+  },
+  {
+    id: 72,
+    cityName: "충청남도 홍성군",
+  },
+  {
+    id: 73,
+    cityName: "충청남도 부여군",
+  },
+  {
+    id: 74,
+    cityName: "충청남도 서천군",
+  },
+  {
+    id: 75,
+    cityName: "충청남도 금산군",
+  },
+  {
+    id: 76,
+    cityName: "충청남도 청양군",
+  },
+  {
+    id: 77,
+    cityName: "충청북도 청주시",
+  },
+  {
+    id: 78,
+    cityName: "충청북도 충주시",
+  },
+  {
+    id: 79,
+    cityName: "충청북도 제천시",
+  },
+  {
+    id: 80,
+    cityName: "충청북도 음성군",
+  },
+  {
+    id: 81,
+    cityName: "충청북도 진천군",
+  },
+  {
+    id: 82,
+    cityName: "충청북도 단양군",
+  },
+  {
+    id: 83,
+    cityName: "충청북도 증평군",
+  },
+  {
+    id: 84,
+    cityName: "충청북도 괴산군",
+  },
+  {
+    id: 85,
+    cityName: "충청북도 보은군",
+  },
+  {
+    id: 86,
+    cityName: "충청북도 옥천군",
+  },
+  {
+    id: 87,
+    cityName: "충청북도 청원군",
+  },
+  {
+    id: 88,
+    cityName: "충청북도 영동군",
+  },
+  {
+    id: 89,
+    cityName: "전라남도 나주시",
+  },
+  {
+    id: 90,
+    cityName: "전라남도 목포시",
+  },
+  {
+    id: 91,
+    cityName: "전라남도 순천시",
+  },
+  {
+    id: 92,
+    cityName: "전라남도 여수시",
+  },
+  {
+    id: 93,
+    cityName: "전라남도 광양시",
+  },
+  {
+    id: 94,
+    cityName: "전라남도 함평군",
+  },
+  {
+    id: 95,
+    cityName: "전라남도 신안군",
+  },
+  {
+    id: 96,
+    cityName: "전라남도 무안군",
+  },
+  {
+    id: 97,
+    cityName: "전라남도 곡성군",
+  },
+  {
+    id: 98,
+    cityName: "전라남도 담양군",
+  },
+  {
+    id: 99,
+    cityName: "전라남도 장성군",
+  },
+  {
+    id: 100,
+    cityName: "전라남도 영광군",
+  },
+  {
+    id: 101,
+    cityName: "전라남도 영양군",
+  },
+  {
+    id: 102,
+    cityName: "전라남도 해남군",
+  },
+  {
+    id: 103,
+    cityName: "전라남도 진도군",
+  },
+  {
+    id: 104,
+    cityName: "전라남도 완도군",
+  },
+  {
+    id: 105,
+    cityName: "전라남도 보성군",
+  },
+  {
+    id: 106,
+    cityName: "전라남도 구례군",
+  },
+  {
+    id: 107,
+    cityName: "전라남도 장흥군",
+  },
+  {
+    id: 108,
+    cityName: "전라남도 강진군",
+  },
+  {
+    id: 109,
+    cityName: "전라남도 화순군",
+  },
+  {
+    id: 110,
+    cityName: "전라남도 고흥군",
+  },
+  {
+    id: 111,
+    cityName: "전라북도 군산시",
+  },
+  {
+    id: 112,
+    cityName: "전라북도 익산시",
+  },
+  {
+    id: 113,
+    cityName: "전라북도 김제시",
+  },
+  {
+    id: 114,
+    cityName: "전라북도 전주시",
+  },
+  {
+    id: 115,
+    cityName: "전라북도 정읍시",
+  },
+  {
+    id: 116,
+    cityName: "전라북도 남원시",
+  },
+  {
+    id: 117,
+    cityName: "전라북도 부안군",
+  },
+  {
+    id: 118,
+    cityName: "전라북도 고창군",
+  },
+  {
+    id: 119,
+    cityName: "전라북도 순창군",
+  },
+  {
+    id: 120,
+    cityName: "전라북도 임실군",
+  },
+  {
+    id: 121,
+    cityName: "전라북도 장수군",
+  },
+  {
+    id: 122,
+    cityName: "전라북도 무주군",
+  },
+  {
+    id: 123,
+    cityName: "전라북도 완주군",
+  },
+  {
+    id: 124,
+    cityName: "전라북도 진안군",
+  },
+  {
+    id: 125,
+    cityName: "경상남도 진주시",
+  },
+  {
+    id: 126,
+    cityName: "경상남도 밀양시",
+  },
+  {
+    id: 127,
+    cityName: "경상남도 양산시",
+  },
+  {
+    id: 128,
+    cityName: "경상남도 김해시",
+  },
+  {
+    id: 129,
+    cityName: "경상남도 사천시",
+  },
+  {
+    id: 130,
+    cityName: "경상남도 통영시",
+  },
+  {
+    id: 131,
+    cityName: "경상남도 거제시",
+  },
+  {
+    id: 132,
+    cityName: "경상남도 창원시",
+  },
+  {
+    id: 133,
+    cityName: "경상남도 거창군",
+  },
+  {
+    id: 134,
+    cityName: "경상남도 함양군",
+  },
+  {
+    id: 135,
+    cityName: "경상남도 합천군",
+  },
+  {
+    id: 136,
+    cityName: "경상남도 의령군",
+  },
+  {
+    id: 137,
+    cityName: "경상남도 창녕군",
+  },
+  {
+    id: 138,
+    cityName: "경상남도 산청군",
+  },
+  {
+    id: 139,
+    cityName: "경상남도 하동군",
+  },
+  {
+    id: 140,
+    cityName: "경상남도 함안군",
+  },
+  {
+    id: 141,
+    cityName: "경상남도 고성군",
+  },
+  {
+    id: 142,
+    cityName: "경상남도 남해군",
+  },
+  {
+    id: 143,
+    cityName: "경상북도 안동시",
+  },
+  {
+    id: 144,
+    cityName: "경상북도 문경시",
+  },
+  {
+    id: 145,
+    cityName: "경상북도 상주시",
+  },
+  {
+    id: 146,
+    cityName: "경상북도 김천시",
+  },
+  {
+    id: 147,
+    cityName: "경상북도 구미시",
+  },
+  {
+    id: 148,
+    cityName: "경상북도 영천시",
+  },
+  {
+    id: 149,
+    cityName: "경상북도 경산시",
+  },
+  {
+    id: 150,
+    cityName: "경상북도 경주시",
+  },
+  {
+    id: 151,
+    cityName: "경상북도 포항시",
+  },
+  {
+    id: 152,
+    cityName: "경상북도 영주시",
+  },
+  {
+    id: 153,
+    cityName: "경상북도 울진군",
+  },
+  {
+    id: 154,
+    cityName: "경상북도 영양군",
+  },
+  {
+    id: 155,
+    cityName: "경상북도 영덕군",
+  },
+  {
+    id: 156,
+    cityName: "경상북도 청송군",
+  },
+  {
+    id: 157,
+    cityName: "경상북도 봉화군",
+  },
+  {
+    id: 158,
+    cityName: "경상북도 예천군",
+  },
+  {
+    id: 159,
+    cityName: "경상북도 칠곡군",
+  },
+  {
+    id: 160,
+    cityName: "경상북도 의성군",
+  },
+  {
+    id: 161,
+    cityName: "경상북도 성주군",
+  },
+  {
+    id: 162,
+    cityName: "경상북도 군위군",
+  },
+  {
+    id: 163,
+    cityName: "경상북도 청도군",
+  },
+  {
+    id: 164,
+    cityName: "경상북도 고령군",
+  },
+];
