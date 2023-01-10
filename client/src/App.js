d1f28e573685f1ebdb1e1c5082e963e51801380d
uk11 <dlckddnrr@gmail.com>
2023-01-10 13:19:08 +0900

    [Test]: test

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
-import {useState} from "react";
-import {Route, Routes} from "react-router-dom";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return (
-        <Routes>
-            <Route path="/" element={isLoggedIn ? <Main/> : <Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-            <Route path="/user/:memberId" element={<UserProfile/>}/>
-            <Route path="/user/:memberId/edit" element={<UserProfileEdit/>}/>
-            <Route path="/itinerary/:itineraryId" element={<Itinerary/>}/>
-            {/*<Route path="/blog" element={<Blog/>}/>*/}
-        </Routes>
+      <>
+        <GlobalStyle />
+          <div className="app">
+            <Routes>
+              <Route path="/" element={isLoggedIn ? <Main/> : <Home/>}/>
+              <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="/signup" element={<SignUpPage setIsLoggedIn={setIsLoggedIn}/>}/>
+              <Route path="/user/:memberId" element={<UserProfile/>}/>
+              <Route path="/user/:memberId/edit" element={<UserProfileEdit/>}/>
+              <Route path="/itinerary/:itineraryId" element={<Itinerary/>}/>
+              {/* <Route path="/blog" element={<Blog/>}/> */}
+            </Routes>
+          </div>
+      </>
     );
 }
 
