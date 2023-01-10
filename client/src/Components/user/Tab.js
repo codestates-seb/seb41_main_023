05f605604a5475c21511a3c84e6a384b1308f63f
sena <senika_@naver.com>
2023-01-10 13:51:22 +0900

    feat: token setting

--- client/src/Components/user/Tab.js
+++ client/src/Components/user/Tab.js
@@ -1,3 +1,5 @@
+/* 유저이름, 비밀번호 수정, 계정 삭제 */
+
 import axios from "axios";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
@@ -8,6 +10,16 @@ import Modal from "./Modal";
 // const [token, setToken] = useState();
 //const [memberId,setMemberId] = useState();
 
+//토큰 설정
+// useEffect(() => {
+//   if (cookies.accessToken) {
+//     setToken(cookies.accessToken.token)
+//   }
+// }, []);
+
+//memberId 설정
+
+/* 유저이름 수정 */
 const General = ({ handleChange, handleSubmit, nameRef }) => {
   return (
     <GeneralContainer>
@@ -27,6 +39,7 @@ const General = ({ handleChange, handleSubmit, nameRef }) => {
   );
 };
 
+/* 비밀번호 수정 */
 const Password = () => {
   //기존 비밀번호와 일치하는지 확인해야함
   const [inputs, setInputs] = useState({
@@ -45,10 +58,26 @@ const Password = () => {
   };
 
   const submitPassword = () => {
+    //0. origin password가 현재 비밀번호와 일치해야 함
+
+    //1. 기존 비밀번호와 달라야 함
+    // if (inputs.originPassword === inputs.newPassword) {
+    //   return window.alert("기존 비밀번호와 다른 비밀번호를 입력해주세요.");
+    // }
+
+    //유효성 검사(숫자, 영문, 특수문자 조합한 8~20자리)
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&#]{8,}$/i;
+
+    const passwordValueCheck = passwordRegex.test(inputs.newPassword);
+    if (!passwordValueCheck) {
+      return;
+    }
     console.log("change!");
-    //새 비밀번호 유효성 검사 필요
+
+    /* 비밀번호 변경 요청 */
     // axios
-    //   .patch(`${process.env.REACT_APP_API_URL}/members/password/1`, {
+    //   .patch(`${process.env.REACT_APP_API_URL}/members/password/${memberId}`, {
     //     headers: {
     //       Authorization: token,
     //       withCredentials: true,
@@ -60,6 +89,10 @@ const Password = () => {
     //   })
     //   .then((res) => {
     //     alert("비밀번호가 변경되었습니다.");
+    // setInputs({
+    //   originPassword: "",
+    //   newPassword: "",
+    // });
     //     window.location.reload();
     //   })
     //   .catch((err) => console.log("error"));
@@ -83,8 +116,8 @@ const Password = () => {
           onChange={handleChange}
         ></input>
         <div>
-          Passwords must contain at least eight characters, including at least 1
-          letter and 1 number.
+          비밀번호는 8자 이상이어야 하며, 숫자, 영문, 특수문자를 모두 포함해야
+          합니다.
         </div>
       </div>
       <div className="submit_area">
@@ -94,6 +127,7 @@ const Password = () => {
   );
 };
 
+/* 계정 삭제 */
 const DeleteAccount = ({ modal, setModal }) => {
   const navigate = useNavigate();
   const handleDeleteAccount = () => {
@@ -105,7 +139,7 @@ const DeleteAccount = ({ modal, setModal }) => {
     //     },
     //   })
     //   .then((res) => {
-    //     // removeCookie("쿠키 이름");
+    //     removeCookie("쿠키 이름");
     //     alert("그동안 이용해주셔서 감사합니다.");
     //     navigate("/");
     //     window.location.reload();
