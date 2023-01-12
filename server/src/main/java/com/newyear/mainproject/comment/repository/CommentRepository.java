8f17eb8514e46c976d4d081f88a0ee240f8f1a3e
kimmj13 <qlckadk@gmail.com>
2023-01-12 11:08:08 +0900

    chore : 댓글 - 오타수정 #22

--- server/src/main/java/com/newyear/mainproject/comment/repository/CommentRepository.java
+++ server/src/main/java/com/newyear/mainproject/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Page<Comment> findAllByboard(Pageable pageable, Board board);
+    Page<Comment> findAllByBoard(Pageable pageable, Board board);
 }
