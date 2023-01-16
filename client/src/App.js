c5f4f51b6c237d0cd1c2072b2b7748b8812efff1
seungheonkim <seungheon818@naver.com>
2023-01-17 00:31:25 +0900

    feat:게시물 페이지 마커 이동 추가

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ import LoginPage from "./Pages/LoginPage";
 import SignUpPage from "./Pages/SignUpPage";
 import UserProfile from "./Pages/UserProfile";
 import UserProfileEdit from "./Pages/UserProfileEdit";
-// import WriteBoard from "./Pages/WriteBoard";
-// import WriteSingleBoard from "./Pages/WriteSingleBoard";
+import WriteBoard from "./Pages/WriteBoard";
+import WriteSingleBoard from "./Pages/WriteSingleBoard";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -42,13 +42,13 @@ function App() {
         {/*  /!*서비스 이용자들의 모든 게시글이 노출되는 전체 게시글 목록 페이지*!/*/}
         {/*<Route path="/board" element={<Board/>}/>*/}
         {/*  /!*게시글 작성 시작을 위한 여행선택 페이지*!/*/}
-        {/*<Route path="/board/plan" element={<WriteBoard/>}/>*/}
+        <Route path="/board/plan" element={<WriteBoard/>}/>
         {/*  /!*특정 게시글 조회한 페이지*!/*/}
         {/*<Route path="/board/:boardId" element={<SingleBoard/>}/>*/}
         {/*  /!*게시글 수정 페이지*!/*/}
         {/*<Route path="/board//edit/:boardId" element={<EditSingleBoard/>}/>*/}
         {/*  /!*게시글 작성 페이지*!/*/}
-        {/*<Route path="/board/plan/:planId" element={<WriteSingleBoard/>}/>*/}
+        <Route path="/board/plan/:planId" element={<WriteSingleBoard/>}/>
       </Routes>
     </>
   );
