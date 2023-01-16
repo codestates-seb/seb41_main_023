3fb93e79285f176c1018bc5c1c01000de7c98e38
phant <phantom4060@gmail.com>
2023-01-16 20:20:33 +0900

    feat: Password와 DisplayName 유효성 검증/ 가입되어 있지 않은 회원, 탈퇴한 회원 로그인, 로그인 안한 회원 로그아웃 예외처리 추가

--- server/src/main/java/com/newyear/mainproject/security/handler/MemberAuthenticationFailureHandler.java
+++ server/src/main/java/com/newyear/mainproject/security/handler/MemberAuthenticationFailureHandler.java
@@ -2,8 +2,8 @@ package com.newyear.mainproject.security.handler;
 
 import com.google.gson.Gson;
 import com.newyear.mainproject.exception.ErrorResponse;
+import com.newyear.mainproject.exception.ExceptionCode;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -21,14 +21,23 @@ public class MemberAuthenticationFailureHandler implements AuthenticationFailure
         // 인증 실패 시, 에러 로그를 기록하거나 error response를 전송할 수 있다.
         log.error("# Authentication failed: {}", exception.getMessage());
 
-        sendErrorResponse(response);
+        sendErrorResponse(response, exception);
     }
 
-    private void sendErrorResponse(HttpServletResponse response) throws IOException {
+    private void sendErrorResponse(HttpServletResponse response, AuthenticationException exception) throws IOException {
+        ExceptionCode exceptionCode = ExceptionCode.UNAUTHORIZED;
+
+        if(exception.getMessage() == "Member not found"){
+            exceptionCode = ExceptionCode.UNAUTHORIZED; //프론트쪽에서 401로 해서 맞춰주기
+        }
+        if(exception.getMessage() == "Invalid member status"){
+            exceptionCode = ExceptionCode.INVALID_MEMBER_STATUS;
+        }
+
         Gson gson = new Gson();
-        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED);
+        ErrorResponse errorResponse = ErrorResponse.of(exceptionCode);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setStatus(exceptionCode.getStatus());
         response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
     }
 }
