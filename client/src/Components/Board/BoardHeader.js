7796a79f986a96946399a1ccd90553ff13ac765d
sena <senika_@naver.com>
2023-01-17 00:10:24 +0900

    feat: board 페이지 추가

---
+++ client/src/Components/Board/BoardHeader.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+
+const BoardHeader = ({ mainData, mode }) => {
+  // console.log(mainData);
+  const navigate = useNavigate();
+  return (
+    <TopNavBar>
+      <LogoButtonContainer>
+        <Logo>
+          <img
+            alt="logo_image"
+            src="https://picsum.photos/40"
+            onClick={() => navigate("/")}
+          />
+        </Logo>
+        {mode === "write" ? (
+          <SaveButton>Save travel log</SaveButton>
+        ) : (
+          <>
+            <SaveButton>Save edits</SaveButton>
+            <SaveButton>Delete</SaveButton>
+          </>
+        )}
+      </LogoButtonContainer>
+      <TripTitleContainer>
+        <h1>{mainData.cityName}</h1>
+        <h1>{mainData.planTitle}</h1>
+        {moment(mainData.startDate).format("M월 D일")} ~
+        {moment(mainData.endDate).format("M월 D일")}
+      </TripTitleContainer>
+    </TopNavBar>
+  );
+};
+
+export default BoardHeader;
+
+const TopNavBar = styled.nav`
+  border-bottom: 1px solid #e9ecef;
+  margin: 20px;
+  display: flex;
+  left: 0;
+  right: 0;
+  top: 0;
+  padding: 0;
+  z-index: 50;
+  background-color: white !important;
+  flex-flow: column nowrap;
+`;
+
+const LogoButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  cursor: pointer;
+
+  img {
+    margin-right: 15px;
+    border-radius: 50%;
+  }
+`;
+
+const SaveButton = styled.button`
+  display: flex;
+  align-items: center;
+  margin: 0 20px;
+  padding: 5px 10px;
+  border-radius: 2rem;
+  background-color: #4f4f4f;
+  color: white;
+`;
+
+const TripTitleContainer = styled.div`
+  width: auto;
+`;
---
+++ client/src/Pages/WriteSingleBoard.js
@@ -0,0 +1,156 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+import { getCookie } from "../Util/Cookies";
+import {
+  LoadScript,
+  GoogleMap,
+  MarkerF,
+  InfoWindowF,
+} from "@react-google-maps/api";
+
+import BoardHeader from "../Components/Board/BoardHeader";
+
+const WriteSingleBoard = () => {
+  const { planId } = useParams();
+  const [mainData, setMainData] = useState();
+  const [planDatesAndPlace, setPlanDatesAndPlace] = useState();
+  const token = getCookie("accessToken");
+
+  console.log(mainData);
+
+  //map 관련
+  const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
+  const [center, setCenter] = useState({
+    lat: 37.555969,
+    lng: 126.972336,
+  });
+
+  const [searchData, setSearchData] = useState({
+    placeName: "",
+    placeAddress: "",
+  });
+
+  const [libraries] = useState(["places"]);
+
+  const onLoad = (marker) => {
+    console.log("marker: ", marker);
+  };
+
+  const mapContainerStyle = {
+    width: "50%",
+    minWidth: "400px",
+    position: "absolute",
+    height: "100%",
+    zIndex: "60",
+    top: "0",
+    bottom: "0",
+    right: "0",
+  };
+
+  const [geocode, setGeocode] = useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  //선택한 일정 조회
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/plans/${planId}`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        // console.log(res.data.data);
+        setMainData({
+          cityName: res.data.data.cityName,
+          planTitle: res.data.data.planTitle,
+          startDate: res.data.data.startDate,
+          endDate: res.data.data.endDate,
+          budgetId: res.data.data.budget.budgetId,
+          planDates: res.data.data.planDates,
+          planDatesAndPlace: res.data.data.planDatesAndPlace,
+        });
+        setPlanDatesAndPlace(res.data.data.planDatesAndPlace);
+        const startPlace = res.data.data.planDatesAndPlace;
+        console.log(startPlace);
+        setSearchData({
+          placeName: startPlace[0].places[0].placeName,
+          placeAddress: startPlace[0].places[0].placeAddress,
+        });
+        setGeocode({
+          lat: startPlace[0].places[0].latitude,
+          lng: startPlace[0].places[0].longitude,
+        });
+      });
+  }, []);
+
+  const handleGeoCode = (lat, lng) => {
+    setGeocode({ lat, lng });
+  };
+
+  const InfoData = () => {
+    return (
+      <>
+        <div>{searchData.placeName}</div>
+        <div>{searchData.placeAddress}</div>
+      </>
+    );
+  };
+
+  return (
+    <>
+      {mainData && <BoardHeader mainData={mainData} mode="write" />}
+      <MemoBox>
+        <h3>Travel experience</h3>
+        <textarea placeholder="Share your experience!"></textarea>
+      </MemoBox>
+      <ItineraryBox>
+        <h3>Itinerary</h3>
+        {mainData &&
+          planDatesAndPlace.map((el) => (
+            <div key={el.planId}>
+              <div>{el.planDate}</div>
+              {el.places.map((place) => (
+                <div
+                  key={place.planDateId}
+                  onClick={() => {
+                    setSearchData(place);
+                    handleGeoCode(place.latitude, place.longitude);
+                  }}
+                >
+                  <div>{place.placeName}</div>
+                  <div>{place.placeAddress}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+      </ItineraryBox>
+      <MapBox>
+        <LoadScript googleMapsApiKey={API_KEY} libraries={libraries}>
+          <GoogleMap
+            zoom={15}
+            center={center}
+            mapContainerStyle={mapContainerStyle}
+          >
+            <MarkerF onLoad={onLoad} position={geocode} scale={5}>
+              <InfoWindowF position={geocode}>
+                <InfoData />
+              </InfoWindowF>
+            </MarkerF>
+          </GoogleMap>
+        </LoadScript>
+      </MapBox>
+    </>
+  );
+};
+
+export default WriteSingleBoard;
+
+const MemoBox = styled.div``;
+const ItineraryBox = styled.div``;
+const MapBox = styled.div``;
