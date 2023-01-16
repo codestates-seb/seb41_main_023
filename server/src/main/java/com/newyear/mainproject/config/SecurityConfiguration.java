de2a6da8954cce9312373c5c34896c1db270557a
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2023-01-16 15:07:40 +0900

    [BE] 게시판에 장소 세부 항목 추가 (#91)
    * [Feat] : 공통기능 초기 셋팅
    
    * feat : 예산/비용 CRUD
    
    * style : 예산과 비용 코드 스타일 변경 #10
    
    * Update ExpenseDto.java
    
    * style : 예산과 비용 코드 스타일 변경 #10
    
    * style : 예산/비용 url 형식 통일, reponse에 생성일 추가 #10
    
    * style : 예산/비용 url 형식 통일, reponse에 생성일 추가 #10
    
    * feat : 장소 - description 세팅 기능 추가 #14
    
    * feat : 게시판 CRUD
    
    * feat : 댓글 CRUD
    
    * feat : 좋아요 기능 추가
    
    * feat : 회원 탈퇴시 게시글, 댓글 삭제 기능 추 #9
    
    * feat : 비용에 카테고리 항목 추가 #10
    
    * feat : 비용에 카테고리 항목 추가 #10
    
    * feat : 회원 프로필 이미지 기능 추가 #24
    
    * chore : 게시물/댓글 - 회원 프로필 이미지와 연동 #24
    
    * chore : 댓글 - 오타수정 #22
    
    * chore : 회원 - 프로필 이미지 jpg/jpeg/png 만 허용하도록 처리 #34
    
    * chore : city - 대표 이미지 데이터 추가 #34
    
    * chore : board - 도시 이미지 항목 응답에 추가 #22
    
    * fix : member - 기본 프로필 이미지 삭제 오류 보 #34
    
    * chore: 소셜로그인 토큰 반환 #9
    
    * chore: 게시판 도시로 조회시의 옵션 추가 #22
    
    * chore: s3 - 날짜형식 오타수정 #61
    
    * chore: 소셜 로그인 토큰 반환 url 변 #76
    
    * chore: 소셜 로그인 토큰 반환 url 변 #76
    
    * style: expense service 폴더 추가 #25
    
    * style: url 수정
    
    * chore: 예외 처리 추가
    
    * commit
    
    * chore : 게시판에 장소 세부 항목 추 #22
    
    * chore : 게시판에 장소 세부 항목 추 #22

--- server/src/main/java/com/newyear/mainproject/config/SecurityConfiguration.java
+++ server/src/main/java/com/newyear/mainproject/config/SecurityConfiguration.java
@@ -59,10 +59,10 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                                .antMatchers(HttpMethod.POST, "/members/signup", "/members/login", "/login/**").permitAll()
+                                .antMatchers(HttpMethod.POST, "/members/signup", "/members/login", "/login/**", "/email/auth").permitAll()
                                 .antMatchers(HttpMethod.PATCH, "/members/**").hasRole("USER")
                                 .antMatchers(HttpMethod.POST, "/members/logout").permitAll()
-                                .antMatchers(HttpMethod.GET, "/members").hasAnyRole("ADMIN", "USER")
+                                .antMatchers(HttpMethod.GET, "/members", "/board/plan/**").hasAnyRole("ADMIN", "USER")
                                 .antMatchers(HttpMethod.GET, "/", "/members/**", "/city", "/board", "/board/**", "/comments/**").permitAll() //추후 추가하기
                                 .antMatchers(HttpMethod.DELETE, "/members/**").hasRole("USER")
                                 .antMatchers("/h2/**").permitAll() // h2 콘솔 사용을 위한 설정
