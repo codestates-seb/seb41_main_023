bfad007933b6fe968047f61e78ae667942d25ff0
Yujeu07 <111178684+Yujeu07@users.noreply.github.com>
2023-01-16 20:41:44 +0900

    feat: Password와 DisplayName 유효성 검증/ 가입되어 있지 않은 회원, 탈퇴한 회원 로그인, 로그인 안한 회원 로그아웃 예외처리 추가 (#94)

--- server/src/main/java/com/newyear/mainproject/exception/ExceptionCode.java
+++ server/src/main/java/com/newyear/mainproject/exception/ExceptionCode.java
@@ -53,7 +53,8 @@ public enum ExceptionCode {
     INVALID_MEMBER_STATUS(400, "Invalid member status"),
     MEMBER_EXISTS(409, "Member exists"),
     MEMBER_NOT_FOUND(404, "Member not found"),
-    INVALID_PASSWORD (400, "Invalid Password");
+    INVALID_PASSWORD (400, "Invalid Password"),
+    MEMBER_NOT_LOGIN(400, "Member not login");
 
     @Getter
     private int status;
