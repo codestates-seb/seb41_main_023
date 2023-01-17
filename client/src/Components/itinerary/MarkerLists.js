db2e3bd32a8ecfc1e30159c657cf6029039d5881
seungheonkim <seungheon818@naver.com>
2023-01-18 05:25:04 +0900

    feat:세부 게시글 조회 페이지 구현(뎃글 기능 미포함)

--- client/src/Components/itinerary/MarkerLists.js
+++ client/src/Components/itinerary/MarkerLists.js
@@ -27,7 +27,6 @@ const MarkerLists = (props) => {
         if (day === '금') return "skyblue"
         if (day === '토') return "gray"
         if (day === '일') return "pink"
-
     }
 
     const markerUrl = `data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="1.5rem" height="2rem" viewBox="0 0 384 512"%3E%3Cpath fill="${makeColor(`${moment(data.planDate).format('ddd')}`)}" d="M172.268 501.67C26.97 291.031 0 269.413 0 192C0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67c-9.535 13.774-29.93 13.773-39.464 0z"%2F%3E%3C%2Fsvg%3E`;
