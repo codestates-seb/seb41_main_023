bcf7354136542c00c8f0cf6e275a0d543536c234
ashleysyheo <ashleysyheo@gmail.com>
2023-01-18 21:10:26 +0900

    refactor: Category input을 dropdown(select)로 변경

--- client/src/Components/budget/Category.js
+++ client/src/Components/budget/Category.js
@@ -1,121 +1,144 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Category = ({ setCategory, handleCategory }) => {
   //카테고리 목록
   const categoryList = [
-    "Flight",
-    "Lodging",
-    "Car Rental",
-    "Public transit",
-    "Food",
-    "Drink",
-    "Sightseeing",
-    "Activities",
-    "Shopping",
-    "Gas",
-    "Groceries",
-    "Other",
+    'Flight',
+    'Lodging',
+    'Car Rental',
+    'Public transit',
+    'Food',
+    'Drink',
+    'Sightseeing',
+    'Activities',
+    'Shopping',
+    'Gas',
+    'Groceries',
+    'Other',
   ];
 
   return (
-    <ModalContainer onClick={() => setCategory(false)}>
-      <ModalWrapper onClick={(e) => e.stopPropagation()}>
-        <div className="title_frame">
-          <div className="title">Select Category</div>
-          <div className="cancel_button" onClick={() => setCategory(false)}>
-            ❌
-          </div>
-        </div>
-        <div className="category_list">
-          {categoryList.map((el, idx) => (
-            <div
-              key={idx}
-              className="category_item"
-              onClick={() => handleCategory(el)}
-            >
-              {el}
-            </div>
-          ))}
-        </div>
-      </ModalWrapper>
-    </ModalContainer>
+    <CategoryDropdown>
+      <option value='' disabled selected>
+        항목을 선택하세요
+      </option>
+      {categoryList.map((el, idx) => (
+        <option key={idx} className='category_item' onClick={() => handleCategory(el)}>
+          {el}
+        </option>
+      ))}
+    </CategoryDropdown>
+    // <ModalContainer onClick={() => setCategory(false)}>
+    //   <ModalWrapper onClick={(e) => e.stopPropagation()}>
+    //     <div className="title_frame">
+    //       <div className="title">Select Category</div>
+    //       <div className="cancel_button" onClick={() => setCategory(false)}>
+    //         ❌
+    //       </div>
+    //     </div>
+    //     <div className="category_list">
+    //       {categoryList.map((el, idx) => (
+    //         <div
+    //           key={idx}
+    //           className="category_item"
+    //           onClick={() => handleCategory(el)}
+    //         >
+    //           {el}
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </ModalWrapper>
+    // </ModalContainer>
   );
 };
 
 export default Category;
 
-const ModalContainer = styled.div`
-  background: rgba(0, 0, 0, 0.5);
-  /* display: none; */
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 10000;
-`;
+const CategoryDropdown = styled.select`
+  padding: 10px 14px;
+  width: 100%;
+  background-color: white;
+  border: 1px solid var(--light-gray-4);
+  border-radius: 3px;
+  cursor: pointer;
 
-const ModalWrapper = styled.div`
-  position: absolute;
-  overflow: hidden;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: 100%;
-  max-width: 500px;
-  z-index: 1050;
-
-  display: flex;
-  flex-direction: column;
-
-  padding: 24px;
-  font-size: 13px;
-  line-height: 17px;
-  background-color: #fff;
-  border-radius: 7px;
-  box-shadow: rgba(0, 0, 0, 0.09) 0px 1px 4px 0px,
-    rgba(0, 0, 0, 0.09) 0px 3px 8px 0px, rgba(0, 0, 0, 0.13) 0px 4px 13px 0px;
-  > .title_frame {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin-bottom: 20px;
-    width: 432px;
-    height: 31px;
-    font-size: 13px;
-    > .title {
-      color: #c22e32;
-      font-size: 27px;
-      line-height: 32px;
-      font-weight: 400;
-    }
-    > .cancel_button {
-      width: 13px;
-      height: 13px;
-      color: #6a737c;
-      cursor: pointer;
-      margin-top: 4px;
-    }
-  }
-  > .category_list {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    > .category_item {
-      margin: 5px;
-      padding: 10px;
-      border: 1px solid #6a737c;
-      border-radius: 5px;
-      background-color: snow;
-
-      width: 120px;
-      height: 40px;
-
-      text-align: center;
-      white-space: nowrap;
-
-      cursor: pointer;
-    }
+  > option[value=''][disabled] {
+    display: none;
   }
 `;
+
+// const ModalContainer = styled.div`
+//   background: rgba(0, 0, 0, 0.5);
+//   /* display: none; */
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+//   z-index: 10000;
+// `;
+
+// const ModalWrapper = styled.div`
+//   position: absolute;
+//   overflow: hidden;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   max-width: 100%;
+//   max-width: 500px;
+//   z-index: 1050;
+
+//   display: flex;
+//   flex-direction: column;
+
+//   padding: 24px;
+//   font-size: 13px;
+//   line-height: 17px;
+//   background-color: #fff;
+//   border-radius: 7px;
+//   box-shadow: rgba(0, 0, 0, 0.09) 0px 1px 4px 0px, rgba(0, 0, 0, 0.09) 0px 3px 8px 0px,
+//     rgba(0, 0, 0, 0.13) 0px 4px 13px 0px;
+//   > .title_frame {
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: space-between;
+//     margin-bottom: 20px;
+//     width: 432px;
+//     height: 31px;
+//     font-size: 13px;
+//     > .title {
+//       color: #c22e32;
+//       font-size: 27px;
+//       line-height: 32px;
+//       font-weight: 400;
+//     }
+//     > .cancel_button {
+//       width: 13px;
+//       height: 13px;
+//       color: #6a737c;
+//       cursor: pointer;
+//       margin-top: 4px;
+//     }
+//   }
+//   > .category_list {
+//     display: flex;
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     justify-content: center;
+//     > .category_item {
+//       margin: 5px;
+//       padding: 10px;
+//       border: 1px solid #6a737c;
+//       border-radius: 5px;
+//       background-color: snow;
+
+//       width: 120px;
+//       height: 40px;
+
+//       text-align: center;
+//       white-space: nowrap;
+
+//       cursor: pointer;
+//     }
+//   }
+// `;
