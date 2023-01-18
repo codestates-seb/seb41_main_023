f90f6bb51e20c1da2cc9fdd38c47af344a762699
ashleysyheo <ashleysyheo@gmail.com>
2023-01-18 23:22:47 +0900

    css: 날짜 선택 dropdown css 수정

--- client/src/Components/itinerary/PlanDropDown.js
+++ client/src/Components/itinerary/PlanDropDown.js
@@ -8,56 +8,35 @@ export const Container = styled.div`
   min-width: 77px;
 `;
 
-const activeExist = ({ active = true }) => {
-  return `max-height: ${active ? '100px' : '0'}`;
-};
-
 export const StyledOptionList = styled.ul`
-  box-sizing: border-box; // 테두리에 맞게 ul 크기 조절
-  position: absolute; // absolute를 이용해 위치를 원하는 곳에 둘것.
-  top: 2.6rem; // 그곳에 위에서부터 2.6rem(드롭다운본체의 크기)만큼 떨어진 곳.
-  list-style-type: none; // ul을 커스텀할 거라면 꼭 해줘야하는 list-style-type:none
-  width: 120%; // 크기는 드롭다운 본체의 너비와 동일하게함.
-  border-radius: 8px; // 동글동글하게 아래부분을 만들어야해서 border-radius를 줌.
-  background: #ffffff; // 배경색
-  ${activeExist};
-  transition: 0.2s ease-in-out; // 0.2초를 걸려서 부드럽게 ul이 보이고 사라진다.
-  overflow-y: scroll; // scroll을 통해 리스트 내용들을 보겠다.
-  &::-webkit-scrollbar {
-    // scrollbar 자체의 설정
-    // 너비를 작게 설정
-    width: 6px;
-  }
-  &::-webkit-scrollbar-track {
-    // scrollbar의 배경부분 설정
-    // 부모와 동일하게 함(나중에 절전모드, 밤모드 추가되면 수정하기 번거로우니까... 미리 보이는 노동은 최소화)
-    background: transparent;
-  }
-  &::-webkit-scrollbar-thumb {
-    // scrollbar의 bar 부분 설정
-    // 동글동글한 회색 바를 만든다.
-    border-radius: 1rem;
-    background: transparent;
-  }
-  &::-webkit-scrollbar-button {
-    // scrollbar의 상하단 위/아래 이동 버튼
-    // 크기를 안줘서 안보이게 함.
-    width: 0;
-    height: 0;
-  }
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  margin-top: var(--spacing-2);
+  width: 100%;
+  background-color: var(--white);
+  max-height: 200px;
+  overflow-y: scroll;
+  border-radius: 3px;
+  box-shadow: 0px 0px 1px rgba(9, 30, 66, 0.31), 0px 4px 6px rgba(9, 30, 66, 0.15);
+  list-style: none;
+  transition: all 0.1s ease-in;
+  display: ${(props) => (props.active ? 'block' : 'none')};
 `;
 
 export const StyledOptionItem = styled.li`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  margin: 7px 3px;
-  color: #0f0f0f;
-  font-size: 12px;
+  width: 100%;
+  padding: 12px;
+  text-align: center;
+  cursor: pointer;
 
   &:hover {
-    background: #ffffff;
+    background: var(--light-gray-2);
+    color: var(--dark-gray-1);
+  }
+
+  &:active {
+    background: var(--light-gray-3);
   }
 `;
 
