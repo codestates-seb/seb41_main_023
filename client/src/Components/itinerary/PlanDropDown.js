f7e6aab24df1a27dffd8766586d7244d25fad956
ashleysyheo <ashleysyheo@gmail.com>
2023-01-18 21:01:39 +0900

    css: 일정 페이지 장소 검색 버튼 css 수정

--- client/src/Components/itinerary/PlanDropDown.js
+++ client/src/Components/itinerary/PlanDropDown.js
@@ -1,45 +1,15 @@
-import styled from "styled-components";
-import {Fragment, useState} from "react";
-import SingleDate from "./SingleDate";
-import moment from "moment";
+import styled from 'styled-components';
+import { Fragment, useState } from 'react';
+import SingleDate from './SingleDate';
+import moment from 'moment';
 
-const ListWrapper = styled.div`
-  //height: 30px;
-`;
-
-
-export const StyledSelectBox = styled.div`
+export const Container = styled.div`
   position: relative;
-  width: 10rem;
-  height: 2.6rem;
-  border-radius: 8px;
-  background: #ffffff;
-  cursor: pointer;
-`;
-
-export const StyledSelectedLabel = styled.button`
-  // display설정
-  display: flex; 
-  align-items: center;
-  border: none;
-  // 크기 설정
-  box-sizing: border-box; 
-  width: inherit;
-  height: inherit;
-  // more 아이콘과 가까운 위치에 글자를 두기 위함.
-  justify-content: center;
-  //padding-right: 2.5rem;
-  // 글자 크기 설정
-  font-size: 1rem;
-  // more아이콘 설정
-  background: calc(100% - 0.5rem) center no-repeat;
-  background-size: 2rem;
-  // 커서가 올라오면 pointer모양으로 변경
-  cursor: pointer;
+  min-width: 77px;
 `;
 
 const activeExist = ({ active = true }) => {
-    return `max-height: ${active ? '100px' : '0'}`;
+  return `max-height: ${active ? '100px' : '0'}`;
 };
 
 export const StyledOptionList = styled.ul`
@@ -50,24 +20,28 @@ export const StyledOptionList = styled.ul`
   width: 120%; // 크기는 드롭다운 본체의 너비와 동일하게함.
   border-radius: 8px; // 동글동글하게 아래부분을 만들어야해서 border-radius를 줌.
   background: #ffffff; // 배경색
-  ${activeExist}; 
+  ${activeExist};
   transition: 0.2s ease-in-out; // 0.2초를 걸려서 부드럽게 ul이 보이고 사라진다.
   overflow-y: scroll; // scroll을 통해 리스트 내용들을 보겠다.
-  &::-webkit-scrollbar { // scrollbar 자체의 설정
-  	// 너비를 작게 설정
-    width: 6px; 
+  &::-webkit-scrollbar {
+    // scrollbar 자체의 설정
+    // 너비를 작게 설정
+    width: 6px;
   }
-  &::-webkit-scrollbar-track { // scrollbar의 배경부분 설정
-  	// 부모와 동일하게 함(나중에 절전모드, 밤모드 추가되면 수정하기 번거로우니까... 미리 보이는 노동은 최소화)
+  &::-webkit-scrollbar-track {
+    // scrollbar의 배경부분 설정
+    // 부모와 동일하게 함(나중에 절전모드, 밤모드 추가되면 수정하기 번거로우니까... 미리 보이는 노동은 최소화)
     background: transparent;
   }
-  &::-webkit-scrollbar-thumb { // scrollbar의 bar 부분 설정
-  	// 동글동글한 회색 바를 만든다.
+  &::-webkit-scrollbar-thumb {
+    // scrollbar의 bar 부분 설정
+    // 동글동글한 회색 바를 만든다.
     border-radius: 1rem;
     background: transparent;
   }
-  &::-webkit-scrollbar-button { // scrollbar의 상하단 위/아래 이동 버튼
-  	// 크기를 안줘서 안보이게 함.
+  &::-webkit-scrollbar-button {
+    // scrollbar의 상하단 위/아래 이동 버튼
+    // 크기를 안줘서 안보이게 함.
     width: 0;
     height: 0;
   }
@@ -81,51 +55,46 @@ export const StyledOptionItem = styled.li`
   margin: 7px 3px;
   color: #0f0f0f;
   font-size: 12px;
-  
+
   &:hover {
     background: #ffffff;
   }
 `;
 
 const dropDownSvg = (
-    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet"
-         viewBox="0 0 24 24">
-        <path fill="currentColor" d="m7 10l5 5l5-5z"/>
-    </svg>
-)
+  <svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid meet' viewBox='0 0 24 24'>
+    <path fill='currentColor' d='m7 10l5 5l5-5z' />
+  </svg>
+);
 
 const PlanDropDown = (props) => {
-    const {singlePlanData, setSelectedDateId} = props;
-    const [selected, setSelected] = useState('일정선택')
-    const [isShow, setIsShow] = useState(false);
+  const { singlePlanData, setSelectedDateId } = props;
+  const [selected, setSelected] = useState('날짜 선택');
+  const [isShow, setIsShow] = useState(false);
 
-    const showMenu = () => {
-        setIsShow(prevState => !prevState);
-    };
+  const showMenu = () => {
+    setIsShow((prevState) => !prevState);
+  };
 
-    return (
-        <StyledSelectBox>
-            <StyledSelectedLabel
-                value={selected}
-                onClick={showMenu}
-            >
-                {selected}
-            </StyledSelectedLabel>
-            <StyledOptionList active={isShow}>
-                {singlePlanData.map((data) => (
-                        <SingleDate
-                            key={data.planDateId}
-                            id={data.planDateId}
-                            data={data}
-                            setIsShow={setIsShow}
-                            setSelected={setSelected}
-                            setSelectedDateId={setSelectedDateId}
-                            />
-                    )
-                )}
-            </StyledOptionList>
-        </StyledSelectBox>
-    )
+  return (
+    <Container>
+      <button className='button--primary' value={selected} onClick={showMenu}>
+        {selected}
+      </button>
+      <StyledOptionList active={isShow}>
+        {singlePlanData.map((data) => (
+          <SingleDate
+            key={data.planDateId}
+            id={data.planDateId}
+            data={data}
+            setIsShow={setIsShow}
+            setSelected={setSelected}
+            setSelectedDateId={setSelectedDateId}
+          />
+        ))}
+      </StyledOptionList>
+    </Container>
+  );
 };
 
 export default PlanDropDown;
