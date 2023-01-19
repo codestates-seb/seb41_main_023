caa5edf4a6bb3f1eb4e27b0dad8a07efb6f46e0c
ashleysyheo <ashleysyheo@gmail.com>
2023-01-19 22:37:14 +0900

    refactor: map pin icon 수정 및 color, stroke, label color 요일에 따라 다른 색상 적용

--- client/src/Components/itinerary/MarkerLists.js
+++ client/src/Components/itinerary/MarkerLists.js
@@ -16,18 +16,40 @@ const MarkerLists = (props) => {
   };
 
   const makeColor = (day) => {
-    if (day === '월') return 'dodgerblue';
-    if (day === '화') return 'blueviolet';
-    if (day === '수') return 'deeppink';
-    if (day === '목') return 'darkorange';
+    if (day === '월') return 'deepskyblue';
+    if (day === '화') return 'orchid';
+    if (day === '수') return 'lightcoral';
+    if (day === '목') return 'orange';
     if (day === '금') return 'gold';
-    if (day === '토') return 'limegreen';
+    if (day === '토') return 'mediumseagreen';
+    if (day === '일') return 'turquoise';
+  };
+
+  const makeStrokeColor = (day) => {
+    if (day === '월') return 'dodgerblue';
+    if (day === '화') return 'mediumorchid';
+    if (day === '수') return 'indianred';
+    if (day === '목') return 'darkorange';
+    if (day === '금') return 'goldenrod';
+    if (day === '토') return 'seagreen';
     if (day === '일') return 'darkturquoise';
   };
 
-  const markerUrl = `data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="1.5rem" height="2rem" viewBox="0 0 384 512"%3E%3Cpath fill="${makeColor(
+  const makeLabelColor = (day) => {
+    if (day === '월') return '#115496';
+    if (day === '화') return '#7d1596';
+    if (day === '수') return '#731717';
+    if (day === '목') return '#804600';
+    if (day === '금') return '#8a6508';
+    if (day === '토') return '#08572a';
+    if (day === '일') return '#006a6b';
+  };
+
+  const markerUrl = `data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="64" height="64" viewBox="0 -5 64 64"%3E%3Cpath fill="${makeColor(
     `${moment(data.planDate).format('ddd')}`
-  )}" d="M172.268 501.67C26.97 291.031 0 269.413 0 192C0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67c-9.535 13.774-29.93 13.773-39.464 0z"%2F%3E%3C%2Fsvg%3E`;
+  )}" stroke="${makeStrokeColor(
+    `${moment(data.planDate).format('ddd')}`
+  )}"  stroke-width="2" d="M32.9034 57.1083C33.1835 56.9665 33.4262 56.7608 33.612 56.5078C44.536 41.6478 50 31.0358 50 24.6758C50 14.6758 42 6.67578 32 6.67578C22 6.67578 14 14.6758 14 24.6758C14 31.0358 19.464 41.6478 30.388 56.5078C30.5738 56.7608 30.8165 56.9665 31.0966 57.1083C31.3766 57.2501 31.6861 57.324 32 57.324C32.3139 57.324 32.6234 57.2501 32.9034 57.1083Z"%2F%3E%3C%2Fsvg%3E`;
 
   return (
     <Fragment>
@@ -45,7 +67,10 @@ const MarkerLists = (props) => {
                   handleZoom();
                 }}
                 // label={`${moment(data.planDate).format('ddd')}`}
-                label={(idx + 1).toString()}
+                label={{
+                  text: (idx + 1).toString(),
+                  color: makeLabelColor(`${moment(data.planDate).format('ddd')}`),
+                }}
                 options={{
                   icon: {
                     url: markerUrl,
