3d28194712c1978e5f28391986796c3bcbafb8b7
ashleysyheo <ashleysyheo@gmail.com>
2023-01-19 22:40:44 +0900

    css: 일정 페이지 side date bar css & html 수정

--- client/src/Components/itinerary/SideDateBar.js
+++ client/src/Components/itinerary/SideDateBar.js
@@ -19,12 +19,19 @@ const LeftSideBar = styled.div`
 
 const DateBox = styled.div`
   > button {
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
     padding: var(--spacing-2);
     width: 100%;
-    font-size: var(--large-text-size);
-    line-height: var(--large-text-line-height);
     border-radius: 3px;
     background-color: transparent;
+
+    > span {
+      font-size: var(--large-text-size);
+      line-height: var(--large-text-line-height);
+      color: inherit;
+    }
 
     &:hover {
       color: var(--primary-blue);
@@ -44,7 +51,10 @@ const SideDateBar = (props) => {
       <h5>Dates</h5>
       {singlePlanData.map((date) => (
         <DateBox key={date.planDateId}>
-          <button>{moment(date.planDate).format('M.D')}</button>
+          <button>
+            <span>{moment(date.planDate).format('M.D')}</span>
+            <span>{moment(date.planDate).format('ddd')}</span>
+          </button>
         </DateBox>
       ))}
     </LeftSideBar>
