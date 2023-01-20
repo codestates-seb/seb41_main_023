0dc9c11961d41ad2458921b39728c7fb744f3054
ashleysyheo <ashleysyheo@gmail.com>
2023-01-20 18:32:55 +0900

    css: 게시글 페이지 지도 css 수정

--- client/src/Components/Board/SingleBoard/MapSection.js
+++ client/src/Components/Board/SingleBoard/MapSection.js
@@ -1,48 +1,47 @@
-import {useState} from "react";
-import {GoogleMap, LoadScript} from "@react-google-maps/api";
-import {Markers} from "./Markers";
+import { useState } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { Markers } from './Markers';
 
 const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 const mapContainerStyle = {
-    width: "50vw",
-    minWidth: "400px",
-    position: "absolute",
-    height: "100vh",
-    zIndex: "60",
-    top: "0",
-    bottom: "0",
-    right: "0",
+  position: 'fixed',
+  width: '50vw',
+  height: '100vh',
+  zIndex: '60',
+  top: '0',
+  right: '0',
 };
 export const MapSection = (props) => {
-    const {boardData, geocode, handleGeoCode} = props;
-    const [libraries] = useState(['places']);
-    const [isZoom, setIsZoom] = useState(13);
-    const [isOpen, setIsOpen] = useState(false);
+  const { boardData, geocode, handleGeoCode } = props;
+  const [libraries] = useState(['places']);
+  const [isZoom, setIsZoom] = useState(13);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleZoom = () => {
-        setIsZoom(17);
-    }
+  const handleZoom = () => {
+    setIsZoom(17);
+  };
 
-    return (
-        <LoadScript googleMapsApiKey={API_KEY} libraries={libraries}>
-            <GoogleMap
-                id={"board-map"}
-                mapContainerStyle={mapContainerStyle}
-                zoom={isZoom}
-                center={geocode}
-            >
-                {boardData ? (
-                    boardData.days.map((data, idx) => (
-                        <Markers
-                            key={idx}
-                            data={data}
-                            handleGeoCode={handleGeoCode}
-                            handleZoom={handleZoom}
-                            isOpen={isOpen}
-                            setIsOpen={setIsOpen}
-                        />
-                    ))) : null}
-            </GoogleMap>
-        </LoadScript>
-    )
+  return (
+    <LoadScript googleMapsApiKey={API_KEY} libraries={libraries}>
+      <GoogleMap
+        id={'board-map'}
+        mapContainerStyle={mapContainerStyle}
+        zoom={isZoom}
+        center={geocode}
+      >
+        {boardData
+          ? boardData.days.map((data, idx) => (
+              <Markers
+                key={idx}
+                data={data}
+                handleGeoCode={handleGeoCode}
+                handleZoom={handleZoom}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+              />
+            ))
+          : null}
+      </GoogleMap>
+    </LoadScript>
+  );
 };
