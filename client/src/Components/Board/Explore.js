44764bdfc82b6d521a72c5cabb4311ef2d92363f
uk11 <dlckddnrr@gmail.com>
2023-01-25 12:15:54 +0900

    feat: 무한 스크롤 로딩, 딜레이 추가

--- client/src/Components/Board/Explore.js
+++ client/src/Components/Board/Explore.js
@@ -1,133 +1,202 @@
 import styled from "styled-components";
 import axios from "axios";
-import {useCallback, useEffect, useRef, useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {getCookie} from "../../Util/Cookies";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getCookie } from "../../Util/Cookies";
 import moment from "moment";
 
 const Explore = (props) => {
-        const [exploreList, setExploreList] = useState([]);
-        const token = getCookie("accessToken");
-        const navigate = useNavigate();
-        const page = useRef(1);
-        const [hasNextPage, setHasNextPage] = useState(true);
-        const observerTargetEl = useRef(null);
+  const [exploreList, setExploreList] = useState([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-        const fetchMoreExplores = useCallback(async () => {
-            await axios
-                .get(`${process.env.REACT_APP_API_URL}/board?page=${page.current}&size=5&tab=likes`, {
-                    headers: {
-                        Authorization: token,
-                    },
-                })
-                .then((res) => {
-                    console.log(res.data.data)
-                    setExploreList(prevState => [...prevState, ...res.data.data])
-                    setHasNextPage(res.data.data.length === 5);
-                    if (res.data.data.length) page.current += 1;
-                })
-                .catch(err => console.log(err));
-        }, [page.current]);
+  const token = getCookie("accessToken");
+  const navigate = useNavigate();
 
-        useEffect(() => {
-                if (props.searches) {
-                    axios.get(`${process.env.REACT_APP_API_URL}/board?page=1&size=100&tab=boardId&city=${props.destination}`, {
-                        headers: {
-                            Authorization: token,
-                        },
-                    })
-                        .then((res) => {
-                            setExploreList(res.data.data)
-                        })
-                        .catch(err => console.log(err));
-                } else {
-                    if(window.location.pathname === '/board') {
-                        if (!observerTargetEl.current || !hasNextPage) return;
+  const observerTargetEl = useRef(null);
+  const page = useRef(2);
 
-                        const io = new IntersectionObserver((entries, observer) => {
-                            if (entries[0].isIntersecting) {
-                                fetchMoreExplores();
-                            }
-                        });
-                        io.observe(observerTargetEl.current);
+  // 무한 스크롤
+  const fetchMoreExplores = useCallback(async () => {
+    setLoading(true);
+    await axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/board?page=${page.current}&size=5&tab=likes`,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      )
+      .then((res) => {
+        setTimeout(() => {
+          setExploreList((prevState) => [...prevState, ...res.data.data]);
+          setLoading(false);
+        }, 2000);
+        setHasNextPage(res.data.data.length === 5);
+        if (res.data.data.length) page.current += 1;
+      })
+      .catch((err) => console.log(err));
+  }, [page.current]);
 
-                        return () => {
-                            io.disconnect();
-                        }
-                    } else {
-                        axios.get(`${process.env.REACT_APP_API_URL}/board?page=1&size=5&tab=likes`, {
-                            headers: {
-                                Authorization: token,
-                            },
-                        })
-                            .then((res) => {
-                                setExploreList(res.data.data)
-                            })
-                            .catch((err) => console.log(err));
-                    }
-                }
-            }, [fetchMoreExplores, hasNextPage, token, props.destination]
-        );
+  useEffect(() => {
+    // 게시판 접근 시
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/board?page=1&size=5&tab=likes`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        setExploreList(res.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-        const handleNavigate = (explore) => {
-            navigate(`/board/${explore.boardId}`);
+  useEffect(() => {
+    // 검색
+    if (props.searches) {
+      axios
+        .get(
+          `${process.env.REACT_APP_API_URL}/board?page=1&size=100&tab=boardId&city=${props.destination}`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        )
+        .then((res) => {
+          setExploreList(res.data.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      // 게시판
+      if (window.location.pathname === "/board") {
+        if (!observerTargetEl.current || !hasNextPage) return;
+        const options = { threshold: 1.0 };
+
+        const io = new IntersectionObserver((entries, observer) => {
+          if (entries[0].isIntersecting) {
+            fetchMoreExplores();
+          }
+        }, options);
+        io.observe(observerTargetEl.current);
+
+        return () => {
+          io.disconnect();
         };
-
-        return (
-            <ExploreContainer>
-                <div className="contents">
-                    {exploreList.length !== 0 ?
-                        (exploreList.map((explore, index) => (
-                            <div
-                                className="my-logs__card"
-                                key={index}
-                                onClick={() => handleNavigate(explore)}
-                            >
-                                <img
-                                    className="meta__travel-image"
-                                    alt="place_image"
-                                    src={explore.cityImage}
-                                />
-                                <div className="meta_title">{explore.title}</div>
-                                <div className="meta_content">
-                                    {moment(explore.travelPeriod.split("-")[0]).format("M월 D일")} -{" "}
-                                    {moment(explore.travelPeriod.split("-")[1]).format("M월 D일")}
-                                </div>
-                                <div className="meta_profile">
-                                    <img
-                                        className="profile__image"
-                                        alt="profile_image"
-                                        src={explore.profileImage}
-                                    />
-                                    <span>{explore.displayName} </span>
-                                </div>
-                                <div
-                                    className={
-                                        explore.checkLikes ? "meta_likes likes" : "meta_likes"
-                                    }
-                                >
-                                    <svg viewBox="0 0 16 16">
-                                        <path
-                                            fillRule="evenodd"
-                                            fill="currentColor"
-                                            d="M7.29583817,13.7871612 C7.68473613,14.1808512 8.31605486,14.1828078 8.70304958,13.7885531 C8.70304958,13.7885531 10.9002368,11.6291175 13,9.00215315 C15,6.50000023 15.5000002,3.49999998 13,2.00000001 C10.5031852,0.501911222 8.00000022,3.00000005 8.00000022,3.00000005 C8.00000022,3.00000005 5.49772957,0.501362336 3.00000005,2.00000001 C0.500000019,3.49999999 0.999999993,6.50000023 2.99999999,9.00215315 C5.09401769,11.6219294 7.29583817,13.7871612 7.29583817,13.7871612 Z"
-                                        ></path>
-                                    </svg>
-                                </div>
-                            </div>
-                        ))) : <div className={'search__error'}>검색어와 일치하는 게시글이 없습니다</div>}
-                    <div ref={observerTargetEl}/>
-                </div>
-            </ExploreContainer>
-        );
+      } else {
+        // 메인
+        axios
+          .get(
+            `${process.env.REACT_APP_API_URL}/board?page=1&size=100&tab=likes`,
+            {
+              headers: {
+                Authorization: token,
+              },
+            }
+          )
+          .then((res) => {
+            setExploreList(res.data.data);
+          })
+          .catch((err) => console.log(err));
+      }
     }
-;
+  }, [fetchMoreExplores, hasNextPage, token, props.destination]);
 
+  const handleNavigate = (explore) => {
+    navigate(`/board/${explore.boardId}`);
+  };
+
+  return (
+    <ExploreContainer>
+      <div className="contents">
+        {exploreList.length !== 0 ? (
+          exploreList.map((explore, index) => (
+            <div
+              className="my-logs__card"
+              key={index}
+              onClick={() => handleNavigate(explore)}
+            >
+              <img
+                className="meta__travel-image"
+                alt="place_image"
+                src={explore.cityImage}
+              />
+              <div className="meta_title">{explore.title}</div>
+              <div className="meta_content">
+                {moment(explore.travelPeriod.split("-")[0]).format("M월 D일")} -{" "}
+                {moment(explore.travelPeriod.split("-")[1]).format("M월 D일")}
+              </div>
+              <div className="meta_profile">
+                <img
+                  className="profile__image"
+                  alt="profile_image"
+                  src={explore.profileImage}
+                />
+                <span>{explore.displayName} </span>
+              </div>
+              <div
+                className={
+                  explore.checkLikes ? "meta_likes likes" : "meta_likes"
+                }
+              >
+                <svg viewBox="0 0 16 16">
+                  <path
+                    fillRule="evenodd"
+                    fill="currentColor"
+                    d="M7.29583817,13.7871612 C7.68473613,14.1808512 8.31605486,14.1828078 8.70304958,13.7885531 C8.70304958,13.7885531 10.9002368,11.6291175 13,9.00215315 C15,6.50000023 15.5000002,3.49999998 13,2.00000001 C10.5031852,0.501911222 8.00000022,3.00000005 8.00000022,3.00000005 C8.00000022,3.00000005 5.49772957,0.501362336 3.00000005,2.00000001 C0.500000019,3.49999999 0.999999993,6.50000023 2.99999999,9.00215315 C5.09401769,11.6219294 7.29583817,13.7871612 7.29583817,13.7871612 Z"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className={"search__error"}>
+            검색어와 일치하는 게시글이 없습니다
+          </div>
+        )}
+        {loading ? <div className="loader"></div> : <div></div>}
+        <div ref={observerTargetEl} className="target">
+          target
+        </div>
+      </div>
+    </ExploreContainer>
+  );
+};
 export default Explore;
 
 const ExploreContainer = styled.div`
   position: relative;
   margin-bottom: 50px;
+
+  .target {
+    margin-top: 180px;
+    bottom: 0px;
+    color: white;
+  }
+
+  .loader {
+    width: 48px;
+    height: 48px;
+    border: 5px solid lightgray;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+    margin: 0 auto;
+    margin-top: 50px;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 
   h2 {
     margin-bottom: var(--spacing-4);
@@ -141,7 +210,7 @@ const ExploreContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
     gap: var(--spacing-4);
-    
+
     .search__error {
       width: 1820px;
       display: flex;
