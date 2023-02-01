37034de8c0f7761d9d7b835a7722479fc13e7e32
sena <senika_@naver.com>
2023-02-01 15:23:32 +0900

    Bug:쿠키 수정

--- client/src/Util/Cookies.js
+++ client/src/Util/Cookies.js
@@ -1,15 +1,15 @@
-import { Cookies } from "react-cookie";
+import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
-export const setCookie = (name, value, option) => {
-  return cookies.set(name, value, { ...option });
+export const setCookie = (name, value) => {
+  return cookies.set(name, value, { path: '/' });
 };
 
-export const getCookie = (name) => {
+export const getCookie = name => {
   return cookies.get(name);
 };
 
-export const removeCookie = (name) => {
-  return cookies.remove(name);
+export const removeCookie = name => {
+  return cookies.remove(name, { path: '/' });
 };
