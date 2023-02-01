7e3eb1530f04168582a59f8bebf9caf231d8a153
sena <senika_@naver.com>
2023-02-01 15:48:16 +0900

    Chore: password  수정
    회원가입 시 현재입력한 비밀번호 보이게 가능

--- client/src/Pages/SignUpPage.js
+++ client/src/Pages/SignUpPage.js
@@ -34,6 +34,7 @@ const SignUpPage = () => {
   const [authNum, setAuthNum] = useState('');
   const [isAuth, setIsAuth] = useState(false);
   const [disable, setDisable] = useState(true);
+  const [showPswd, setShowPswd] = useState(false);
 
   // 렌더링 될때 username input으로 focus
   useEffect(() => {
@@ -190,6 +191,10 @@ const SignUpPage = () => {
       });
   };
 
+  const toggleShowPswd = () => {
+    setShowPswd(!showPswd);
+  };
+
   return (
     <>
       <Header>
@@ -263,14 +268,43 @@ const SignUpPage = () => {
           )}
 
           <label>Password</label>
-          <input
-            className="input"
-            type="password"
-            placeholder="Enter your password"
-            onChange={onChangePassword}
-            onKeyDown={pwEnter}
-            ref={pref}
-          />
+          <div className="password--container">
+            <input
+              className="input"
+              type={showPswd ? 'text' : 'password'}
+              placeholder="Enter your password"
+              onChange={onChangePassword}
+              onKeyDown={pwEnter}
+              ref={pref}
+            />
+            <div onClick={toggleShowPswd} className="eye">
+              {showPswd ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5em"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5em"
+                  height="1.5em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M11.83 9L15 12.16V12a3 3 0 0 0-3-3h-.17m-4.3.8l1.55 1.55c-.05.21-.08.42-.08.65a3 3 0 0 0 3 3c.22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53a5 5 0 0 1-5-5c0-.79.2-1.53.53-2.2M2 4.27l2.28 2.28l.45.45C3.08 8.3 1.78 10 1 12c1.73 4.39 6 7.5 11 7.5c1.55 0 3.03-.3 4.38-.84l.43.42L19.73 22L21 20.73L3.27 3M12 7a5 5 0 0 1 5 5c0 .64-.13 1.26-.36 1.82l2.93 2.93c1.5-1.25 2.7-2.89 3.43-4.75c-1.73-4.39-6-7.5-11-7.5c-1.4 0-2.74.25-4 .7l2.17 2.15C10.74 7.13 11.35 7 12 7Z"
+                  />
+                </svg>
+              )}
+            </div>
+          </div>
           {password.length > 0 && (
             <div
               className={`input__message input__message-password message__${
@@ -416,6 +450,15 @@ const LeftContainer = styled.div`
     justify-content: center;
   }
 
+  .password--container {
+    background-color: aliceblue;
+    position: relative;
+    > .eye {
+      position: absolute;
+      top: 25%;
+      left: 90%;
+    }
+  }
   .content {
     width: 350px;
 
